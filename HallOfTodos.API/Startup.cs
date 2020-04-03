e22a7d1868967604b2c9368a2c0c5471239299f1
Dave Harms <daveharmswebdev@gmail.com>
2020-04-03 06:23:47 -0500

    added swagger

--- HallOfTodos.API/Startup.cs
+++ HallOfTodos.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using AutoMapper;
+using Microsoft.OpenApi.Models;
 
 namespace HallOfTodos.API
 {
@@ -42,6 +43,10 @@ namespace HallOfTodos.API
 
             services.AddScoped<ITodoRepository, TodoRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HallOfTodos API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,9 +61,18 @@ namespace HallOfTodos.API
                 app.UseExceptionHandler("/Error");
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "HallOfTodos API v1");
+            });
+
             app.UseStatusCodePages();
 
             app.UseMvc();
+
+            
         }
     }
 }
